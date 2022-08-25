3166367d5ae5b974142e260b73f2cbf12ace9bdc
Kang Suji <sukang0608@naver.com>
2022-08-25 14:58:14 +0900

    네비게이션 메뉴 완료

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  // 사이드 메뉴
+  $(".moreBtn").click(function (e) {
+    const id = e.target.id;
+    if ($(`#${id}.dep2`).hasClass("on")) {
+      $(`#${id}.dep2`).removeClass("on");
+      $(`#${id}-1.dep3`).removeClass("on");
+    } else {
+      $(`#${id}.dep2`).addClass("on");
+    }
+  });
+
+  $(".dep2 > li").each(function () {
+    $(this).mouseenter(function () {
+      $(this).addClass("over");
+    });
+    $(this).mouseleave(function () {
+      $(this).removeClass("over");
+    });
+  });
+
+  $(".dep3 > li").each(function () {
+    $(this).mouseenter(function () {
+      $(this).addClass("over");
+    });
+    $(this).mouseleave(function () {
+      $(this).removeClass("over");
+    });
+  });
+
+  $(".moreBtn2").click(function (e) {
+    const id = e.target.id;
+
+    if ($(`#${id}.dep3`).hasClass("on")) {
+      $(`#${id}.dep3`).removeClass("on");
+    } else {
+      $(`#${id}.dep3`).addClass("on");
+    }
+  });
+  $("#moreBtn5").click(function () {
+    if ($(".login").hasClass("hide")) {
+      $(".login").removeClass("hide");
+    } else {
+      $(".login").addClass("hide");
+    }
+  });
+  $("#moreBtn4").click(function () {
+    if ($(".login").hasClass("hide")) {
+      $(".login").removeClass("hide");
+    } else {
+      $(".login").addClass("hide");
+    }
+  });
+});
