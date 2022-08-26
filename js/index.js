5e09d368b404e28362deef549670f2edfc2353bf
Kang Suji <sukang0608@naver.com>
2022-08-26 17:32:21 +0900

    메인 추가

--- js/index.js
+++ js/index.js
@@ -51,6 +51,14 @@ $(document).ready(function () {
       $(".login").addClass("hide");
     }
   });
+
+  $(".hamburger").click(function () {
+    $(".navigation").css({ width: "50%" });
+  });
+  $(".hamburgerX").click(function () {
+    $(".navigation").css({ width: "0%" });
+  });
+
   var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
@@ -66,5 +74,36 @@ $(document).ready(function () {
     pagination: {
       el: ".img-pagination",
     },
+  });
+  // var swiper = new Swiper(".mySwiper", {
+  //   effect: "coverflow",
+  //   loop: true,
+  //   centeredSlides: true,
+  //   slidesPerView: 2,
+  //   initialSlide: 1,
+  //   keyboardControl: true,
+  //   mousewheelControl: true,
+  //   lazyLoading: true,
+  //   centeredSlides: true,
+  //   preventClicks: false,
+  //   preventClicksPropagation: false,
+  //   lazyLoadingInPrevNext: true,
+  //   coverflow: {
+  //     rotate: 0,
+  //     stretch: 400,
+  //     depth: 200,
+  //     modifier: 1,
+  //     slideShadows: true,
+  //   },
+  // });
+  $(".what-img-wrap").slick({
+    centerMode: true,
+    slidesToShow: 3,
+    initialSlide: 1,
+    infinite: true,
+    swipeToSlide: true,
+    speed: 1300,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
   });
 });
