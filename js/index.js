425681f3b83b7d18202f4540d5ccfb4260906848
Kang Suji <sukang0608@naver.com>
2022-08-30 18:14:57 +0900

    네비게션 수정및 map 수정

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+  $(".bounce img").click(function () {
+    $("html, body").animate({ scrollTop: $(".main-slide").offset().top - 50 }, 500);
+  });
   //search 클릭
   $(".search").click(function () {
     $(".header-search").addClass("active");
@@ -7,24 +10,114 @@ $(document).ready(function () {
     $(".header-search").removeClass("active");
   });
 
-  // 사이드 메뉴
-  $(".moreBtn").click(function (e) {
-    const id = e.target.id;
-    if ($(`#${id}.dep2`).hasClass("on")) {
-      $(`#${id}.dep2`).removeClass("on");
-      $(`#${id}-1.dep3`).removeClass("on");
-    } else {
-      $(`#${id}.dep2`).addClass("on");
-    }
+  $(".dep1-1").on("mouseenter", function (event) {
+    event.preventDefault();
+    $(`.dep1-1 > .dep2`).addClass("on");
+    $(`.dep1-1 > .dep2`).show();
+    $(`.dep1-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-up");
   });
 
+  $(".dep1-1").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep1-1 .dep2`).removeClass("on");
+    $(`.dep1-1 > .dep2`).hide();
+    $(`.dep1-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-down");
+  });
+
+  $(".dep2-1").on("mouseenter", function (event) {
+    event.preventDefault();
+    $(`.dep2-1 > .dep2`).addClass("on");
+    $(`.dep2-1 > .dep2`).show();
+    $(`.dep2-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-up");
+  });
+
+  $(".dep2-1").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep2-1 .dep2`).removeClass("on");
+    $(`.dep2-1 > .dep2`).hide();
+    $(`.dep2-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-down");
+  });
+
+  $(".dep3-1").on("mouseenter", function (event) {
+    event.preventDefault();
+    $(`.dep3-1 > .dep2`).addClass("on");
+    $(`.dep3-1 > .dep2`).show();
+    $(`.dep3-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-up");
+  });
+
+  $(".dep3-1").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep3-1 .dep2`).removeClass("on");
+    $(`.dep3-1 > .dep2`).hide();
+    $(`.dep3-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-down");
+  });
+  $("#more-3").on("mouseenter", function (event) {
+    event.preventDefault();
+    $(`.dep3-3 > .dep3`).addClass("on");
+    $(`.dep3-3 > .dep3`).show();
+    $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
+  });
+
+  $(".dep3").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep3-3 .dep3`).removeClass("on");
+    $(`.dep3-3 > .dep3`).hide();
+    $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-up");
+  });
+  $("#more-4").on("mouseenter", function (event) {
+    event.preventDefault();
+    $(`.dep3-4 > .dep3`).addClass("on");
+    $(`.dep3-4 > .dep3`).show();
+    $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
+  });
+
+  $(".dep3-4").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep3-4 .dep3`).removeClass("on");
+    $(`.dep3-4 > .dep3`).hide();
+    $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-up");
+  });
+  $(".dep4-1").on("mouseenter", function (event) {
+    event.preventDefault();
+    $(`.dep4-1 > .dep2`).addClass("on");
+    $(`.dep4-1 > .dep2`).show();
+    $(`.dep4-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-up");
+  });
+
+  $(".dep4-1").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep4-1 .dep2`).removeClass("on");
+    $(`.dep4-1 > .dep2`).hide();
+    $(`.dep4-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-down");
+  });
+  $(".dep5-1").on("mouseenter", function (event) {
+    event.preventDefault();
+
+    $(`.dep5-1 > .dep2`).addClass("on");
+    $(`.dep5-1 > .dep2`).show();
+    $(`.dep5-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-up");
+  });
+
+  $(".dep5-1").on("mouseleave", function (event) {
+    event.preventDefault();
+    $(`.dep5-1 .dep2`).removeClass("on");
+    $(`.dep5-1 > .dep2`).hide();
+    $(`.dep5-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-down");
+  });
+
+  // 호버 색상적용
+
   $(".dep2 > li").each(function () {
-    $(this).mouseenter(function () {
-      $(this).addClass("over");
-    });
-    $(this).mouseleave(function () {
-      $(this).removeClass("over");
-    });
+    if ($(this).parents().parents().hasClass("dep3-1")) {
+      return;
+    } else {
+      $(this).mouseenter(function () {
+        $(this).addClass("over");
+      });
+      $(this).mouseleave(function () {
+        $(this).removeClass("over");
+      });
+    }
   });
 
   $(".dep3 > li").each(function () {
@@ -34,30 +127,6 @@ $(document).ready(function () {
     $(this).mouseleave(function () {
       $(this).removeClass("over");
     });
-  });
-
-  $(".moreBtn2").click(function (e) {
-    const id = e.target.id;
-
-    if ($(`#${id}.dep3`).hasClass("on")) {
-      $(`#${id}.dep3`).removeClass("on");
-    } else {
-      $(`#${id}.dep3`).addClass("on");
-    }
-  });
-  $("#moreBtn5").click(function () {
-    if ($(".login").hasClass("hide")) {
-      $(".login").removeClass("hide");
-    } else {
-      $(".login").addClass("hide");
-    }
-  });
-  $("#moreBtn4").click(function () {
-    if ($(".login").hasClass("hide")) {
-      $(".login").removeClass("hide");
-    } else {
-      $(".login").addClass("hide");
-    }
   });
 
   $(".hamburger").click(function () {
@@ -81,6 +150,7 @@ $(document).ready(function () {
     },
     pagination: {
       el: ".img-pagination",
+      clickable: true,
     },
   });
 
