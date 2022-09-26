b68949e4ceb9e7a90981a806eca5ea2f952486b9
SUJI <sukang0608@naver.com>
2022-09-26 13:15:33 +0900

    main 1차 완료

--- js/index.js
+++ js/index.js
@@ -24,10 +24,8 @@ $(document).ready(function () {
     //btn-menu-main-m
     if (scroll_pos > $(".main-slide").offset().top - 100) {
       $(".header").addClass("change");
-      $(".main-visual").css({ position: "static" });
     } else if (scroll_pos < $(".main-slide").offset().top - 150) {
       $(".header").removeClass("change");
-      $(".main-visual").css({ position: "sticky" });
     }
   });
 
@@ -78,6 +76,12 @@ $(document).ready(function () {
     event.preventDefault();
     $(`.dep3-1 > .dep2`).addClass("on");
     $(`.dep3-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-up");
+    if ($(`.dep3-3 .moreBtn_2 > span`).hasClass("lnr-chevron-up")) {
+      $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
+    }
+    if ($(`.dep3-4 .moreBtn_2 > span`).hasClass("lnr-chevron-up")) {
+      $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
+    }
   });
 
   $(".dep3-1").on("mouseleave", function (event) {
@@ -85,6 +89,7 @@ $(document).ready(function () {
     $(`.dep3-1 .dep2`).removeClass("on");
     $(`.dep3-1 .moreBtn > span`).attr("class", "lnr lnr-chevron-down");
     $(`.dep3-3 .dep3`).removeClass("on");
+    $(`.dep3-4 .dep3`).removeClass("on");
   });
 
   //dep3
@@ -118,20 +123,20 @@ $(document).ready(function () {
     event.preventDefault();
     if ($(".dep3-3 > .dep3").hasClass("on")) {
       $(`.dep3-3 .dep3`).removeClass("on");
-      $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-up");
+      $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
     } else {
       $(`.dep3-3 > .dep3`).addClass("on");
-      $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
+      $(`.dep3-3 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-up");
     }
   });
   $("#more-4").on("click", function (event) {
     event.preventDefault();
     if ($(".dep3-4 > .dep3").hasClass("on")) {
       $(`.dep3-4 .dep3`).removeClass("on");
-      $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-up");
+      $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
     } else {
       $(`.dep3-4 > .dep3`).addClass("on");
-      $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-down");
+      $(`.dep3-4 .moreBtn_2 > span`).attr("class", "lnr lnr-chevron-up");
     }
   });
 
@@ -197,23 +202,33 @@ $(document).ready(function () {
       el: ".img-pagination",
       clickable: true,
     },
+    navigation: {
+      // 네비게이션 설정
+      nextEl: ".dimNext", // 다음 버튼 클래스명
+      prevEl: ".dimPrev", // 이번 버튼 클래스명
+    },
   });
   $(document).scroll(function () {
     scroll_pos = $(this).scrollTop();
     if (scroll_pos > $(".main-slide").offset().top - 310) {
       $("#img1").css({ animation: "zoom-out 1s linear" });
+      //move01();
+    }
+    if (scroll_pos > $(".map").offset().top - 310) {
+      move01();
     }
   });
 
   //grade 클릭 시 이미지 바꾸기
   $("#Grade10").click(function () {
+    move01();
     $("#img1").css("animation-play-state", "running");
-    const img1Arr = ["KOREA", "FRANCE", "AUSTRALIA", "TAIWAN"];
+    const img1Arr = ["KOREA", "AUSTRALIA", "FRANCE"];
     swiper.slideTo(0, 100);
     $("#Grade10").addClass("btnActive");
     $("#Grade11").removeClass("btnActive");
     $("#Grade12").removeClass("btnActive");
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < 4; i++) {
       $(`#img${i}`).css({ background: `url(./images/grade10_${i}.jpg)` });
       $(`#img${i} .img-dim-text`).text(img1Arr[i - 1]);
     }
@@ -224,13 +239,14 @@ $(document).ready(function () {
     $(".map-tit").text("Take a glance: Grade 10");
   });
   $("#Grade11").click(function () {
+    move_11_01();
     $("#img1").css("animation-play-state", "running");
-    const img2Arr = ["ITALY", "UAE", "MALAYSIA", "ISRAEL"];
+    const img2Arr = ["MEXICO", "ISRAEL", "THAILAND"];
     swiper.slideTo(0, 100);
     $("#Grade11").addClass("btnActive");
     $("#Grade10").removeClass("btnActive");
     $("#Grade12").removeClass("btnActive");
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < 4; i++) {
       $(`#img${i}`).css({ background: `url(./images/grade11_${i}.jpg)` });
       $(`#img${i} .img-dim-text`).text(img2Arr[i - 1]);
     }
@@ -243,13 +259,14 @@ $(document).ready(function () {
     $(".map-tit").text("Take a glance: Grade 11");
   });
   $("#Grade12").click(function () {
+    move_12_01();
     $("#img1").css("animation-play-state", "running");
-    const img3Arr = ["USA", "MEXICO", "KENTA", "CZECH"];
+    const img3Arr = ["USA", "KENYA", "CZECH"];
     swiper.slideTo(0, 100);
     $("#Grade12").addClass("btnActive");
     $("#Grade10").removeClass("btnActive");
     $("#Grade11").removeClass("btnActive");
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i < 4; i++) {
       $(`#img${i}`).css({ background: `url(./images/grade12_${i}.jpg)` });
       $(`#img${i} .img-dim-text`).text(img3Arr[i - 1]);
     }
@@ -260,21 +277,21 @@ $(document).ready(function () {
   });
   //grade 버튼 색상
   if ($("#Grade10").hasClass("btnActive")) {
-    const img1Arr = ["KOREA", "FRANCE", "AUSTRALIA", "TAIWAN"];
-    for (let i = 1; i < 5; i++) {
+    const img1Arr = ["KOREA", "AUSTRALIA", "FRANCE"];
+    for (let i = 1; i < 4; i++) {
       $(`#img${i}`).css({ background: `url(./images/grade10_${i}.jpg)` });
       $(`#img${i} .img-dim-text`).text(img1Arr[i - 1]);
     }
   }
 
-  // for (let i = 1; i < 5; i++) {
-  //   $(`#img${i}`).mouseenter(function () {
-  //     $(`#img${i} .img-dim`).css({ visibility: "visible" });
-  //   });
-  //   $(`#img${i}`).mouseleave(function () {
-  //     $(`#img${i} .img-dim`).css({ visibility: "hidden" });
-  //   });
-  // }
+  for (let i = 1; i < 4; i++) {
+    $(`#img${i}`).mouseenter(function () {
+      $(`#img${i} .img-dim`).css({ visibility: "visible" });
+    });
+    $(`#img${i}`).mouseleave(function () {
+      $(`#img${i} .img-dim`).css({ visibility: "hidden" });
+    });
+  }
 
   //what's on 슬라이드
   $(".what-img-wrap").slick({
@@ -300,3 +317,261 @@ $(document).ready(function () {
     });
   });
 });
+//map.js
+
+spd = 200;
+function move01() {
+  $("#ko1").fadeIn(spd, function () {
+    $(".kodot").fadeIn(spd, function () {
+      $("#ko-line").fadeIn(spd, function () {
+        $("#ko-line").css("height", "14.3%");
+        $("#ko2,#ko3")
+          .delay(500)
+          .fadeIn(spd, function () {
+            $(".ko-text").fadeIn(spd, function () {
+              $(".line_1to2").attr("class", "line_1to2 dash01");
+              move02();
+            });
+          });
+      });
+    });
+  });
+}
+function move02() {
+  $("#aust1")
+    .delay(500)
+    .fadeIn(spd, function () {
+      $(".austdot").fadeIn(spd, function () {
+        $("#aust-line").fadeIn(spd, function () {
+          $("#aust-line").css("width", "6%");
+          $("#aust2,#aust3")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $(".aust-text").fadeIn(spd, function () {
+                $(".line_2to3").attr("class", "line_2to3 dash02");
+                move03();
+              });
+            });
+        });
+      });
+    });
+}
+function move03() {
+  $("#france1")
+    .delay(2000)
+    .fadeIn(spd, function () {
+      $(".francedot").fadeIn(spd, function () {
+        $("#france-line1").fadeIn(spd, function () {
+          $("#france-line1").css("width", "4%");
+          $("#france-line2")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#france-line2").css("height", "32.8%");
+              $("#france2,#france3")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $(".france-text").fadeIn(spd, function () {});
+                });
+            });
+        });
+      });
+    });
+}
+
+/*
+  function move04() {
+    $("#taiwan1")
+      .delay(1000)
+      .fadeIn(spd, function () {
+        $(".taiwandot").fadeIn(spd, function () {
+          $("#taiwan-line").fadeIn(spd, function () {
+            $("#taiwan-line").css("width", "6%");
+            $("#taiwan2,#taiwan3")
+              .delay(500)
+              .fadeIn(spd, function () {
+                $(".taiwan-text").fadeIn(spd, function () {});
+              });
+          });
+        });
+      });
+  }
+*/
+
+function move_11_01() {
+  $("#mexico1")
+    .delay(1000)
+    .fadeIn(spd, function () {
+      $(".mexicodot2").fadeIn(spd, function () {
+        $("#mexico-line1").fadeIn(spd, function () {
+          $("#mexico-line1").css("width", "19%");
+          $("#mexico-line2")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#mexico-line2").css("height", "5.6%");
+              $("#mexico2,#mexico3")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $(".mexico-text").fadeIn(spd, function () {
+                    $(".grade11-line_1to2").attr("class", "grade11-line_1to2 dash01");
+                    move_11_02();
+                  });
+                });
+            });
+        });
+      });
+    });
+}
+function move_11_02() {
+  $("#israel1")
+    .delay(1500)
+    .fadeIn(spd, function () {
+      $(".israedot").fadeIn(spd, function () {
+        $("#italy-line2").fadeIn(spd, function () {
+          $("#italy-line2").css("height", "21%");
+          $("#italy-line1")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#italy-line1").css("width", "10%");
+              $("#israel2,#israel3")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $(".israel-text").fadeIn(spd, function () {
+                    $(".g11_line_2to3").attr("class", "g11_line_2to3 dash01");
+                    move_11_03();
+                  });
+                });
+            });
+        });
+      });
+    });
+}
+function move_11_03() {
+  $("#thai1")
+    .delay(500)
+    .fadeIn(spd, function () {
+      $(".thaidot").fadeIn(spd, function () {
+        $("#thai-line1").fadeIn(spd, function () {
+          $("#thai-line1").css("width", "8%");
+          $("#thai-line2")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#thai-line2").css("height", "15%");
+              $("#thai2, #thai3")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $(".thai-text").fadeIn(spd, function () {
+                    $(".g11_line_3to4").attr("class", "g11_line_3to4 dash01");
+                  });
+                });
+            });
+        });
+      });
+    });
+}
+/*
+  function move_11_04() {
+    $("#israel1").delay(500).fadeIn(spd, function () {
+      $("#israel-line1").fadeIn(spd, function () {
+        $("#israel-line1").css("height", "33%");
+        $("#israel2,#israel3")
+          .delay(500)
+          .fadeIn(spd, function () {
+            $(".israel-text").fadeIn(spd, function () {
+             
+            });
+          });
+      });
+    });
+  }
+*/
+function move_12_01() {
+  $("#usa1,#usa5")
+    .delay(1000)
+    .fadeIn(spd, function () {
+      $(".mexicodot1").fadeIn(spd, function () {
+        $("#usa-line3").fadeIn(spd, function () {
+          $("#usa-line3").css("height", "10%");
+          $("#usa-line2")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#usa-line2").css("width", "5%");
+              $("#usa-line1")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $("#usa-line1").css("height", "6%");
+                  $("#usa2,#usa3")
+                    .delay(500)
+                    .fadeIn(spd, function () {
+                      $(".usa-text").fadeIn(spd, function () {
+                        $(".g12_line_1to2").attr("class", "g12_line_1to2 dash01");
+                        move_12_03();
+                      });
+                    });
+                });
+            });
+        });
+      });
+    });
+}
+
+/*
+    function move_12_02() {
+      $("#mexico-line1").fadeIn(spd, function () {
+	    $("#mexico-line1").css("width", "19%");
+			$("#mexico-line2").delay(500).fadeIn(spd, function () {
+		  $("#mexico-line2").css("height", "5.6%");
+		  $("#mexico2,#mexico3").delay(500).fadeIn(spd, function () {
+			$(".mexico-text").fadeIn(spd, function () {
+				$(".g12_line_2to3").attr("class", "g12_line_2to3 dash01");
+				move_12_03();
+			  });
+			});
+		  });
+		});	  
+	}*/
+
+function move_12_03() {
+  $("#kenya1")
+    .delay(1600)
+    .fadeIn(spd, function () {
+      $(".kenyadot").fadeIn(spd, function () {
+        $("#kenya-line1").fadeIn(100, function () {
+          $("#kenya-line1").css("height", "17.3%");
+          $("#kenya-line2")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#kenya-line2").css("width", "4%");
+              $("#kenya2,#kenya3")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $(".kenya-text").fadeIn(100, function () {
+                    $(".g12_line_3to4").attr("class", "g12_line_3to4 dash02");
+                    move_12_04();
+                  });
+                });
+            });
+        });
+      });
+    });
+}
+function move_12_04() {
+  $("#czech1")
+    .delay(1600)
+    .fadeIn(spd, function () {
+      $(".czechdot").fadeIn(spd, function () {
+        $("#czech-line1").fadeIn(spd, function () {
+          $("#czech-line1").css("height", "17.6%");
+          $("#czech-line2")
+            .delay(500)
+            .fadeIn(spd, function () {
+              $("#czech-line2").css("width", "11%");
+              $("#czech2,#czech3")
+                .delay(500)
+                .fadeIn(spd, function () {
+                  $(".czech-text").fadeIn(spd, function () {});
+                });
+            });
+        });
+      });
+    });
+}
